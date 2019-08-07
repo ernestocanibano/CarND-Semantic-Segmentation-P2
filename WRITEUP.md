9f053bb6b6d16d2b7c5b412aa2e5be99026d2e96
ernesto <ernestoecr@gmail.com>
2019-08-07 15:18:05 +0200

    final project

---
+++ WRITEUP.md
@@ -0,0 +1,66 @@
+# CarND-Semantic-Segmentation-Project
+
+Self-Driving Car Engineer Nanodegree Program, Term3. CarND-Semantic-Segmentation-Project solution by Ernesto Cañibano.
+
+## Training the model
+
+In this section i explain the process of train the model.
+
+### Hyperparameters
+
+After several trainings, finally the hyperparameters selected for the final model are the following:
+* **Dropout = 0.5** [Line 134](./src/main.py#L134)
+* **Learning rate = 1e-5** [Line 135](./src/main.py#L135)
+* **Epochs = 50** [Line 154](./src/main.py#L154)
+* **Batch size = 4** [Line 155](./src/main.py#L155)
+* **Standard deviation = 1e-2** [Line 62](./src/main.py#L62)
+* **L2 regularization in layers = 1e-4** [Line 63](./src/main.py#L63)
+* **L2 regularization additional = 1e-4** [Line 96](./src/main.py#L96)
+ 
+### Training process
+
+The first attempts were made without L2 additional regularization and with a value of the **L2 regularization value in layers** of **1e-5**.
+
+###### 10 epochs
+![](trainning_results/no_regularization/tranning_epochs=10.png) 
+![](trainning_results/no_regularization/epochs=10/um_000000.png)
+![](trainning_results/no_regularization/epochs=10/umm_000050.png) 
+###### 20 epochs
+![](trainning_results/no_regularization/tranning_epochs=20.png) 
+![](trainning_results/no_regularization/epochs=20/um_000000.png)
+![](trainning_results/no_regularization/epochs=20/umm_000050.png) 
+###### 30 epochs
+![](trainning_results/no_regularization/tranning_epochs=30.png) 
+![](trainning_results/no_regularization/epochs=30/um_000000.png)
+![](trainning_results/no_regularization/epochs=30/umm_000050.png) 
+###### 40 epochs
+![](trainning_results/no_regularization/tranning_epochs=40.png) 
+![](trainning_results/no_regularization/epochs=40/um_000000.png)
+![](trainning_results/no_regularization/epochs=40/umm_000050.png) 
+###### 50 epochs
+![](trainning_results/no_regularization/tranning_epochs=50.png) 
+![](trainning_results/no_regularization/epochs=50/um_000000.png)
+![](trainning_results/no_regularization/epochs=50/umm_000050.png) 
+
+A test with **80 epochs** was made too but the result was pretty similar to the 50 epochs test.
+
+Later I tried to improve the model adding an **additional L2 regularization**. I made trainings with different values but the best was with a value of **1e-4**.
+
+###### 50 epochs, 1e-4 additional L2 regularization
+![](trainning_results/regularization/tranning_regularizaction=0.0001-epochs=50_.png) 
+![](trainning_results/regularization/regularizaction=0.0001-epochs=50_/um_000000.png)
+![](trainning_results/regularization/regularizaction=0.0001-epochs=50_/umm_000050.png) 
+
+Finally i tried some more test testing different values for the **Dropout** but I decided use **0.5**.
+
+### Results
+![](runs/1565124891.7792559/um_000007.png)
+![](runs/1565124891.7792559/um_000020.png)
+![](runs/1565124891.7792559/um_000073.png)
+![](runs/1565124891.7792559/uu_000011.png)
+![](runs/1565124891.7792559/uu_000062.png)
+![](runs/1565124891.7792559/uu_000098.png)
+
+
+
+
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import helper
 import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
+import time
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 
 # Check TensorFlow Version
@@ -56,17 +59,19 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    conv_layer7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding= 'same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
-    upsampled_layer7 = tf.layers.conv2d_transpose(conv_layer7, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    stdev = 1e-2
+    l2_reg_value = 1e-4
+    conv_layer7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding= 'same',  kernel_initializer=tf.random_normal_initializer(stddev=stdev), kernel_regularizer=tf.contrib.layers.l2_regularizer(l2_reg_value))
+    upsampled_layer7 = tf.layers.conv2d_transpose(conv_layer7, num_classes, 4, 2, padding='same', kernel_initializer=tf.random_normal_initializer(stddev=stdev), kernel_regularizer=tf.contrib.layers.l2_regularizer(l2_reg_value))
     #print('Input Shape: {}'.format(upsampled_layer7.get_shape()))
     # make sure the shapes are the same!
-    conv_layer4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding= 'same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    conv_layer4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding= 'same', kernel_initializer=tf.random_normal_initializer(stddev=stdev), kernel_regularizer=tf.contrib.layers.l2_regularizer(l2_reg_value))
     skip_layer4_7 = tf.add(upsampled_layer7, conv_layer4)
-    upsampled_layer4_7 = tf.layers.conv2d_transpose(skip_layer4_7, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    upsampled_layer4_7 = tf.layers.conv2d_transpose(skip_layer4_7, num_classes, 4, 2, padding='same', kernel_initializer=tf.random_normal_initializer(stddev=stdev), kernel_regularizer=tf.contrib.layers.l2_regularizer(l2_reg_value))
    
-    conv_layer3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding= 'same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    conv_layer3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding= 'same', kernel_initializer=tf.random_normal_initializer(stddev=stdev), kernel_regularizer=tf.contrib.layers.l2_regularizer(l2_reg_value))
     skip_layer4_7_3 = tf.add(upsampled_layer4_7, conv_layer3)
-    output_layer = tf.layers.conv2d_transpose(skip_layer4_7_3, num_classes, 16, 8, padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    output_layer = tf.layers.conv2d_transpose(skip_layer4_7_3, num_classes, 16, 8, padding='same', kernel_initializer=tf.random_normal_initializer(stddev=stdev), kernel_regularizer=tf.contrib.layers.l2_regularizer(l2_reg_value))
     
     return output_layer
 tests.test_layers(layers)
@@ -82,9 +87,16 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    logits = tf.reshape(nn_last_layer, (-1, num_classes), name='output_layer')
     correct_label = tf.reshape(correct_label, (-1,num_classes))
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+	
+	# add 
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_constant = 1e-4  # Choose an appropriate one.
+    cross_entropy_loss = cross_entropy_loss + reg_constant * sum(reg_losses)
+	
+	# I use an Adam optimizer as is suggested in the classroom video
     train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy_loss)
        
     return logits, train_op, cross_entropy_loss
@@ -109,8 +121,10 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     # TODO: Implement function
     sess.run(tf.global_variables_initializer())
     
-    print('---Starting training---')
+    print('STARTING TRAINNING...')
     print()
+    loss_graph = []
+    init_time = time.time()
     for epoch in range(epochs):
        for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
@@ -120,21 +134,29 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
                                     keep_prob: 0.5,
                                     learning_rate: 0.00001
                                 })
-       print('EPOCH {} of {}...'.format(epoch+1, epochs))
+       loss_graph.append(loss)        
+       print('EPOCH {} / {}...'.format(epoch+1, epochs))
        print('Loss = {:.3f}'.format(loss))
+       print('Elapsed time = {:.2f} s'.format((time.time() - init_time)))
        print()
-    print('---Training finished--')
+    print('TRAINING FINISHED')
+    print('Total elapsed time = {:.2f} s'.format((time.time() - init_time)))
     print()
+	
+	# return losses to plot the trainning
+    return loss_graph
+    
 tests.test_train_nn(train_nn)
 
 
 def run():
     num_classes = 2
-    epochs = 6
-    batch_size = 4
+    epochs = 50
+    batch_size = 8
     image_shape = (160, 576)  # KITTI dataset uses 160x576 images
     data_dir = './data'
     runs_dir = './runs'
+    models_dir = './runs/fcn8_trained_50'
     tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
@@ -154,23 +176,30 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-        print('------BUILD--------')
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
         learning_rate = tf.placeholder(tf.float32, name='learning_rate')
         
         input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
         output_layer = layers(layer3_out, layer4_out, layer7_out, num_classes)
-        print('------OPTIMIZE--------')
         logits, train_op, cross_entropy_loss = optimize(output_layer, correct_label, learning_rate, num_classes)
-
+        
         # TODO: Train NN using the train_nn function
-        print('------TRAIN--------')
-        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
+        losses = train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
+        # create a graphic to evaluate the result of the trainning
+        plt.plot(range(1, len(losses)+1), losses)
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.savefig(runs_dir + "/tranning_"+ str(time.time()) + ".png")
 
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
-
+		
+		# save the trained model
+        saver = tf.train.Saver()
+        saver.save(sess, models_dir + "/fcn8.ckpt")
+        tf.train.write_graph(tf.get_default_graph().as_graph_def(), '',  models_dir + "/base_graph.pb", False)
+        
         # OPTIONAL: Apply the trained model to a video
 
 if __name__ == '__main__':
